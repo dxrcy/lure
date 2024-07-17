c592c4d836d83d79c0b95010ea43de7b427a33a4
darcy <acednes@gmail.com>
2024-07-17 14:23:45 +1000

    values and scope

--- src/interpret.rs
+++ src/interpret.rs
@@ -5,40 +5,136 @@ use crate::{
     lex::{Literal, Number},
     lex_tokens,
     parse::{
-        AccessibleSegment, ChainOrigin, Expr, FuncParams, FuncStatement, Ident, LetStatement,
-        Module, ModuleName, Statement, StatementBody,
+        AccessibleSegment, ChainOrigin, Expr, FuncParams, Ident, LetStatement, ModuleName,
+        Statement, StatementBody,
     },
     parse_module_body, ParseError,
 };
 
-type ModuleMap = HashMap<ModuleName, ModuleValues>;
+#[derive(Debug)]
+struct Context {
+    scope_chain: ScopeChain,
+    modules: ModuleMap,
+}
 
-type ModuleValues = HashMap<Ident, Value>;
+type ScopeChain = Vec<Scope>;
+
+#[derive(Debug, Default)]
+struct Scope {
+    //TODO: Count references
+    values: HashMap<ValueHeapID, ValueHeap>,
+    names: HashMap<String, ValueRef>,
+    //TODO: modules
+    //TODO: templates
+}
+
+type ValueHeapID = usize;
 
 #[derive(Debug)]
 enum Value {
+    Atom(ValueAtom),
+    Heap(ValueHeap),
+}
+
+#[derive(Clone, Copy, Debug)]
+enum ValueRef {
+    Atom(ValueAtom),
+    Reference(ValueHeapID),
+}
+
+#[derive(Clone, Copy, Debug)]
+enum ValueAtom {
     Nil,
     Bool(bool),
     Number(Number),
     Char(char),
+}
+
+#[derive(Debug)]
+enum ValueHeap {
     String(String),
     Table(Table),
     Func(Func),
 }
 
+type ModuleMap = HashMap<ModuleName, ModuleValues>;
+
+type ModuleValues = HashMap<Ident, Value>;
+
+impl From<ValueAtom> for Value {
+    fn from(value: ValueAtom) -> Self {
+        Self::Atom(value)
+    }
+}
+impl From<ValueHeap> for Value {
+    fn from(value: ValueHeap) -> Self {
+        Self::Heap(value)
+    }
+}
+
+impl From<ValueAtom> for ValueRef {
+    fn from(value: ValueAtom) -> Self {
+        Self::Atom(value.into())
+    }
+}
+
 impl From<Literal> for Value {
     fn from(value: Literal) -> Self {
         match value {
-            Literal::Nil => Value::Nil,
-            Literal::Bool(bool) => Value::Bool(bool),
-            Literal::Number(number) => Value::Number(number),
-            Literal::Char(char) => Value::Char(char),
-            Literal::String(string) => Value::String(string),
+            Literal::Nil => ValueAtom::Nil.into(),
+            Literal::Bool(bool) => ValueAtom::Bool(bool).into(),
+            Literal::Number(number) => ValueAtom::Number(number).into(),
+            Literal::Char(char) => ValueAtom::Char(char).into(),
+            Literal::String(string) => ValueHeap::String(string).into(),
         }
     }
 }
 
-impl fmt::Display for Value {
+// impl<T> From<T> for ValueRef
+// where
+//     T: Into<Value>,
+// {
+//     fn from(value: T) -> Self {
+//         ValueRef::Owned(value.into())
+//     }
+// }
+
+// impl Clone for Value {
+//     fn clone(&self) -> Self {
+//         match self {
+//             Value::Nil => Value::Nil,
+//             Value::Bool(bool) => Value::Bool(*bool),
+//             Value::Number(number) => Value::Number(*number),
+//             Value::Char(char) => Value::Char(*char),
+//             _ => panic!("Tried to clone non-copy `Value`. This is wrong I think?"),
+//         }
+//     }
+// }
+
+// impl ValueRef {
+//     pub fn copy_or_reference(&mut self) -> Self {
+//         match self {
+//             // Copy reference
+//             ValueRef::Reference(reference) => ValueRef::Reference(*reference),
+//             ValueRef::Owned(value) => {
+//                 let value = match value {
+//                     // Copy atomic value
+//                     Value::Nil => Value::Nil,
+//                     Value::Bool(bool) => Value::Bool(*bool),
+//                     Value::Number(number) => Value::Number(*number),
+//                     Value::Char(char) => Value::Char(*char),
+//                     // Take reference to owned value
+//                     _ => {
+//                         return ValueRef::Reference(value);
+//                     }
+//                 };
+//                 ValueRef::Owned(value)
+//             }
+//         }
+//     }
+// }
+
+impl fmt::Display for ValueAtom {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Nil => write!(f, "<NIL>"),
@@ -46,9 +142,31 @@ impl fmt::Display for Value {
             Self::Bool(false) => write!(f, "<false>"),
             Self::Number(number) => write!(f, "{}", number),
             Self::Char(char) => write!(f, "'{}'", char),
+        }
+    }
+}
+impl fmt::Display for ValueHeap {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
             Self::String(string) => write!(f, r#""{}""#, string),
             Self::Table(_) => write!(f, "<TABLE>"),
             Self::Func(_) => write!(f, "<FUNC>"),
+        }
+    }
+}
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Atom(value) => write!(f, "{}", value),
+            Self::Heap(value) => write!(f, "{}", value),
+        }
+    }
+}
+impl fmt::Display for ValueRef {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ValueRef::Atom(value) => write!(f, "(atom){}", value),
+            ValueRef::Reference(reference) => write!(f, "(ref){}", reference),
         }
     }
 }
@@ -67,25 +185,43 @@ struct Func {
 }
 
 pub fn interpret_main(file: &str) -> Result<(), InterpretError> {
-    let mut modules = ModuleMap::new();
-    interpret_file(&mut modules, file)?;
+    let scope_chain = ScopeChain::new();
+    let modules = ModuleMap::new();
+    let mut context = Context {
+        scope_chain,
+        modules,
+    };
+
+    interpret_file(&mut context, file)?;
+
     Ok(())
 }
 
-fn interpret_file(modules: &mut ModuleMap, file: &str) -> Result<ModuleValues, InterpretError> {
+fn interpret_file(ctx: &mut Context, file: &str) -> Result<ModuleValues, InterpretError> {
     let body = lex_and_parse(file).map_err(InterpretError::Parse)?;
 
-    let mut module = ModuleValues::new();
+    let scope = Scope::default();
+    ctx.scope_chain.push(scope);
 
     for statement in body {
         match statement {
             Statement::Expr(expr) => {
-                _ = interpret_expr(modules, expr)?;
+                _ = interpret_expr(ctx, expr)?;
             }
 
             Statement::Let(LetStatement { names, value }) => {
-                let value = interpret_expr(modules, value)?;
-                println!("{:?}", value);
+                if !names.rest.is_empty() {
+                    unimplemented!("multiple names in `let` statement");
+                }
+                let name = names.first;
+                let value = interpret_expr(ctx, value)?;
+
+                println!("LET {:?} = {:?}", name, value);
+                let scope = ctx
+                    .scope_chain
+                    .last_mut()
+                    .expect("Should exist because just pushed");
+                scope.names.insert(name, value);
             }
 
             _ => {
@@ -94,14 +230,32 @@ fn interpret_file(modules: &mut ModuleMap, file: &str) -> Result<ModuleValues, I
         }
     }
 
+    let module = ModuleValues::new();
+
     Ok(module)
 }
 
-fn interpret_expr(modules: &mut ModuleMap, expr: Expr) -> Result<Value, InterpretError> {
-    match expr {
-        Expr::Group(inner) => interpret_expr(modules, *inner),
+fn insert_to_heap(ctx: &mut Context, value: ValueHeap) -> ValueRef {
+    let scope = ctx.scope_chain.last_mut().expect("Must have >0 scopes");
+    let id = scope.values.len();
+    scope.values.insert(id, value);
+    ValueRef::Reference(id)
+}
 
-        Expr::Literal(literal) => Ok(literal.into()),
+fn interpret_expr<'a>(ctx: &'a mut Context, expr: Expr) -> Result<ValueRef, InterpretError> {
+    match expr {
+        Expr::Group(inner) => interpret_expr(ctx, *inner),
+
+        Expr::Literal(literal) => {
+            let value: Value = literal.into();
+            match value {
+                Value::Atom(atom) => Ok(atom.into()),
+                Value::Heap(heap) => {
+                    let reference = insert_to_heap(ctx, heap);
+                    Ok(reference)
+                }
+            }
+        }
 
         Expr::Chain(chain) => {
             println!("CHAIN");
@@ -114,30 +268,39 @@ fn interpret_expr(modules: &mut ModuleMap, expr: Expr) -> Result<Value, Interpre
                     panic!("cannot access `print` like that");
                 };
                 for arg in args.args {
-                    let value = interpret_expr(modules, arg)?;
+                    let value = interpret_expr(ctx, arg)?;
                     println!("\x1b[36m{}\x1b[0m", value);
                 }
                 if args.spread_arg.is_some() {
                     unimplemented!("spread args");
                 }
-                return Ok(Value::Nil);
+                return Ok(ValueAtom::Nil.into());
             }
 
-            let ChainOrigin::Name(mut origin) = chain.origin else {
+            let ChainOrigin::Name(origin) = chain.origin else {
                 todo!("access self");
             };
+
+            let Some(mut head) = lookup_variable(ctx, &origin) else {
+                return Err(InterpretError::Run(format!(
+                    "Undefined variable `{}`",
+                    origin
+                )));
+            };
+            println!("ORIGIN: {:?}", head);
+
             for segment in chain.chain {
-                origin = match segment {
+                head = match segment {
                     AccessibleSegment::Name(name) => {
                         todo!("access name.{name}");
                     }
                     AccessibleSegment::Index(index) => {
-                        let index = interpret_expr(modules, index)?;
+                        let index = interpret_expr(ctx, index)?;
                         todo!("access name[{index:?}]");
                     }
                     AccessibleSegment::Slice(start, end) => {
-                        let start = interpret_expr(modules, start)?;
-                        let end = interpret_expr(modules, end)?;
+                        let start = interpret_expr(ctx, start)?;
+                        let end = interpret_expr(ctx, end)?;
                         todo!("access name[{start:?}, {end:?}]");
                     }
                     AccessibleSegment::Call(args) => {
@@ -145,15 +308,26 @@ fn interpret_expr(modules: &mut ModuleMap, expr: Expr) -> Result<Value, Interpre
                     }
                 };
             }
-            println!("chain evaluates to: {:?}", origin);
-            todo!();
+
+            println!("chain evaluates to: {:?}", head);
+            Ok(head)
         }
 
         _ => {
             println!("(todo expr) {:?}", expr);
-            Ok(Value::Nil)
+            Ok(ValueAtom::Nil.into())
         }
     }
+}
+
+fn lookup_variable(ctx: &Context, name: &str) -> Option<ValueRef> {
+    for scope in ctx.scope_chain.iter().rev() {
+        if let Some(value) = scope.names.get(name) {
+            println!("found: {:?}", value);
+            return Some(*value);
+        };
+    }
+    return None;
 }
 
 fn lex_and_parse(file: &str) -> Result<StatementBody, ParseError> {
