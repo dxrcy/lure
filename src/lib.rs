b632093d499604c6bb0699a26d320f4cec3dc33b
darcy <acednes@gmail.com>
2024-10-18 11:22:24 +1100

    temporarily remove interpreter

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,17 @@
 #[macro_use]
 mod debug;
 mod error;
-mod interpret;
+// mod interpret;
 mod lex;
 mod parse;
 mod peek_more;
 mod token_iter;
 
 pub use error::{ParseError, ParseErrorKind};
-pub use interpret::interpret_main;
+// pub use interpret::interpret_main;
 
-use lex::lex_tokens;
-use parse::parse_module_body;
+pub use lex::lex_tokens;
+pub use parse::parse_module_body;
+
 use peek_more::PeekMore;
 use token_iter::TokenIter;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::fs;
 
 fn main() {
-    let filename = "src/example.lur";
+    // let filename = "src/example.lur";
+    let filename = "example/main.lur";
 
     let file = fs::read_to_string(filename).expect("Failed to read file");
 
-    if let Err(error) = lure::interpret_main(&file) {
-        eprintln!("{}", error);
-    };
+    let tokens = lure::lex_tokens(&file).expect("Failed to lex");
+    let body = lure::parse_module_body(tokens).expect("Failed to parse");
+    println!("Successfully parsed.");
+
+    println!("{:#?}", body);
+
+    // if let Err(error) = lure::interpret_main(&file) {
+    //     eprintln!("{}", error);
+    // };
 }
